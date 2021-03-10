d6f480e7f7cbf01f814f1fcc67591e8fdff67348
Julio Machado <juliocarlos00@hotmail.com>
2021-03-10 20:31:44 -0300

    struct/ add styles in repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
@@ -6,7 +5,6 @@ export function App() {
   return (
     <>
       <RepositoryList />;
-      <Counter />
     </>
   );
 }
