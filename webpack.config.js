a4860f7ae383314cea4d0b17edb3371f26cf003d
Julio Machado <juliocarlos00@hotmail.com>
2021-03-09 21:30:40 -0300

    struct/ react

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 		
 module.exports = {	
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),	
     output: {	
         path: path.resolve(__dirname, 'dist'),	
