1052151475cc295a32dce324dba21925ee1b25d3
Julio Machado <juliocarlos00@hotmail.com>
2021-03-09 21:38:29 -0300

    lib/ html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-		
-module.exports = {	
+
+module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),	
-    output: {	
-        path: path.resolve(__dirname, 'dist'),	
-        filename: 'bundle.js'	
-    },	
-    resolve: {	
-        extensions: ['.js', '.jsx'],	
-    },	
-    module: {	
-        rules: [	
-            {	
-                test: /\.jsx$/,	
-                exclude: /node_modules/,	
-                use: 'babel-loader',	
-            }	
-        ],	
-    }	
-}	
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ],
+    }
+}
