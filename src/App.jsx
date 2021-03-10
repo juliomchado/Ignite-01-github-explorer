090c2f03a74dc9732ff91529977f7fb3a3fdb8c4
Julio Machado <juliocarlos00@hotmail.com>
2021-03-09 22:33:48 -0300

    feature/ add RepositoryList dummy component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './styles/global.scss'
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 
 export function App() {
-
-  return <h1>Hello Reactjs</h1>;
+  return <RepositoryList />;
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,43 @@
+const repositoryName = 'unform';
+
+export function RepositoryList(){
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+           
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
